e20fbd0033585f2768ee48ba759dad240e99c176
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-03-27 11:40:59 +0500

    make the all the register logic

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
