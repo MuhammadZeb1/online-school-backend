55cae907118bed813a660e0320323b467d1dab01
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-03-30 15:11:08 +0500

    install the cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";  // <-- import cors
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,10 @@ const app = express();
 
 // middleware
 app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173", // Explicitly allow your frontend
+  credentials: true                // Allow cookies/headers
+}));
 
 // routes
 app.use("/api/auth", authRoutes);
